a1db4e2d9a5bc341306ce2e4835ba24ea23dcfe4
mih-8 <mirian.calistro.souza@escola.pr.gov.br>
2025-09-24 09:22:28 -0300

    .

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
 
+const resultMessage = document.getElementById('result-message');
+const playerChoiceDisplay = document.getElementById('player-choice');
+const computerChoiceDisplay = document.getElementById('computer-choice');
+
+const playerChoiceDisplay = document.getElementById
+const computerChoiceDisplay = document.getElementById
