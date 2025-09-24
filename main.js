e685e81aa87eb4e12c0a7a5a07325b28a0b039bc
mih-8 <mirian.calistro.souza@escola.pr.gov.br>
2025-09-24 20:38:01 -0300

    .

--- main.js
+++ main.js
@@ -6,5 +6,64 @@ const resultMessage = document.getElementById('result-message');
 const playerChoiceDisplay = document.getElementById('player-choice');
 const computerChoiceDisplay = document.getElementById('computer-choice');
 
-const playerChoiceDisplay = document.getElementById
-const computerChoiceDisplay = document.getElementById
+const playerScoreDisplay = document.getElementById('player-score');
+const computerScoreeDisplay = document.getElementById('computer-score');
+
+const resetButton = document.getElementById('reset-button');
+const historyList = document.getElementById('history-list');
+
+let playerScore = 0;
+let computerScore = 0;
+
+const options = ['pedra', 'papel', 'tesoura'];
+
+function playGame(playerChoice) {
+    const computerChoice = options[Math.floor(Math.random() * options.length)];
+
+    playerChoiceDisplay.textContent = 'Sua escolha: ${playerChoice}';
+    computerChoiceDisplay.textContent = 'Escolha do computador: ${computerChoice}';
+
+    let result = '';
+    if (playerChoice === computerChoice) {
+        result = "Empate!";
+    } else if (
+        (playerChoice === 'Pedra' && computerChoice === 'Tesoura') ||
+        (playerChoice === 'Papel' && computerChoice === 'Pedra') ||
+        (playerChoice === 'Tesoura' && computerChoice === 'Papel')
+
+    ) {
+        result = 'Você venceu!';
+        playerScore++;
+    } else {
+        result = 'Você perdeu!';
+        computerScore++;
+    }
+
+resultMessage.textContent = result;
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+
+    addToHistory(playerChoice, computerChoice, result);
+}
+
+function addToHistory(playerChoice, computerChoice, result) {
+    const historyItem = document.createElement('li');
+    historyItem.textContent = 'Você escolheu ${playerChoice}, o computador escolheu ${computerChoice}. Resultado: ${result}';
+    
+    historyList.prepend(historyItem);
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+    resultMessage.textContent = 'Placar reiniciado! Escolha uma opção para começar!';
+    
+    historyList.innerHTML = '';
+}
+
+rockButton.addEventListener('click', () => playGame('Pedra'));
+paperButton.addEventListener('click', () => playGame('Papel'));
+scissorsButton.addEventListener('click', () => playGame('Tesoura'));
+resetButton.addEventListener('click', resetGame);
